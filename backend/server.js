5542e7ae208dbd80f76c402a5cb18076545f97fa
Dhanvaendh M A <104835151+Dhanvaendh-M-A@users.noreply.github.com>
2026-06-09 19:59:17 +0530

    Update CORS origin for Vercel frontend

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ app.use(helmet());
 
 // Allow requests from your Vercel frontend
 app.use(cors({
-  origin: 'https://luxemarket-phi.vercel.app',
+  origin: 'https://luxemarket-r703qgzy6-dhanvaendh-m-a-s-projects.vercel.app',
+  
   credentials: true  // only if you're using cookies/sessions
 }));
 
